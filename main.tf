3c2c8fb06437e61c43ee635383a0d43db082cbe7
michal.browarczyk@zdt.io <michal.browarczyk@zdt.io>
2024-03-19 16:25:01 +0100

    changes precommit

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 resource "digitalocean_loadbalancer" "main" {
-  name                             = var.name
-  region                           = var.region
-  size                             = var.size
-  size_unit                        = var.size_unit
-  vpc_uuid                         = var.vpc_uuid
-  droplet_ids                      = var.droplet_ids
-  droplet_tag                      = var.droplet_tag
+  name        = var.name
+  region      = var.region
+  size        = var.size
+  size_unit   = var.size_unit
+  vpc_uuid    = var.vpc_uuid
+  droplet_ids = var.droplet_ids
+  droplet_tag = var.droplet_tag
 
   dynamic "forwarding_rule" {
     for_each = var.forwarding_rule
     content {
-      entry_port       = forwarding_rule.value.entry_port
-      entry_protocol   = forwarding_rule.value.entry_protocol
-      target_port      = forwarding_rule.value.target_port
-      target_protocol  = forwarding_rule.value.target_protocol
+      entry_port      = forwarding_rule.value.entry_port
+      entry_protocol  = forwarding_rule.value.entry_protocol
+      target_port     = forwarding_rule.value.target_port
+      target_protocol = forwarding_rule.value.target_protocol
     }
   }
-  
+
   dynamic "firewall" {
     for_each = var.firewall
     content {
@@ -24,4 +24,4 @@ resource "digitalocean_loadbalancer" "main" {
       allow = lookup(firewall.value, "allow", null)
     }
   }
-  }
+}
