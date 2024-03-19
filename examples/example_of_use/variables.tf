a59ffedcc1c79045132181c19fa454fa3eb7149f
michal.browarczyk@zdt.io <michal.browarczyk@zdt.io>
2024-03-19 16:39:58 +0100

    var droplet_ids

--- examples/example_of_use/variables.tf
+++ examples/example_of_use/variables.tf
@@ -2,3 +2,9 @@ variable "do_token" {
   description = "Token to digitalOcean API"
   type        = string
 }
+
+variable "droplet_ids" {
+  description = "A list of the IDs of each droplet to be attached to the Load Balancer."
+  type        = list(string)
+  default     = []
+}
