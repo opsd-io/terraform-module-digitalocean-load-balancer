7e1e6ee29f6a3bcefa3c361fa5fe7bb59285a95c
michal.browarczyk@zdt.io <michal.browarczyk@zdt.io>
2024-03-19 16:36:02 +0100

    test

--- examples/example_of_use/main.tf
+++ examples/example_of_use/main.tf
@@ -14,7 +14,7 @@ module "digitalocean_loadbalancer" {
   source      = "github.com/opsd-io/terraform-module-digitalocean-load-balancer?ref=load_balancer_module"
   name        = "loadbalancer-1"
   region      = "nyc1"
-  droplet_ids = [module.digitalocean_droplet.main.id]
+  droplet_ids = [digitalocean_droplet.main.id]
 
 
   forwarding_rule = [
